a373ebd394a0bc5165e50043780dbe151f52365d
jayden <1263346845@qq.com>
2024-04-12 17:24:58 +0800

    axios增加登录相关拦截，请求时候带上token，返回时校验返回码是不是401

--- track_tix_web/src/views/Main.vue
+++ track_tix_web/src/views/Main.vue
@@ -12,7 +12,7 @@
         <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-          Content
+          所有会员总数： {{count}}
         </a-layout-content>
       </a-layout>
     </a-layout>
@@ -22,6 +22,21 @@
 <script setup>
 import Header from "@/components/Header.vue";
 import LayoutSide from "@/components/LayoutSide.vue";
+import axios from "axios";
+import {notification} from "ant-design-vue";
+import {ref} from "vue";
+
+const count = ref(0);
+
+axios.get("/member/member/count").then((response) => {
+  let data = response.data;
+  if (data.success){
+    console.log("count:"+data.content)
+    count.value = data.content;
+  }else{
+    notification.error({ description: data.message });
+  }
+});
 </script>
 
 <style scoped>
