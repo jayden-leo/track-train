43c0f9c922f7f7c8df36c01a1fc913ada6c449a3
jayden <1263346845@qq.com>
2024-04-12 16:21:21 +0800

    配合sessionStorage解决浏览器刷新丢失数据的问题

--- track_tix_web/src/store/index.js
+++ track_tix_web/src/store/index.js
@@ -1,10 +1,14 @@
 import {defineStore} from 'pinia';
 import {ref} from "vue";
 
+const MEMBER = "MEMBER";
 export const useMemberStore = defineStore('MemberStore', () => {
-    let member =ref({})
+    let member = ref(window.SessionStorage.get(MEMBER)||{})
     const setMember = (memberData) => {
         member.value = memberData
+        console.log(member.value)
+        window.SessionStorage.set(MEMBER, memberData)
+
     }
 
     return {member, setMember }
